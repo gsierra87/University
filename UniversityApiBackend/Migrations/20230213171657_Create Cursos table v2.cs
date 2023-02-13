d12f1d6b36075411ca59d748023c9de2db7a3842
Gonzalo Sierra <gonzalo.sierra@arcelormittal.com.ar>
2023-02-13 14:22:15 -0300

    Primera versión

---
+++ UniversityApiBackend/Migrations/20230213171657_Create Cursos table v2.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UniversityApiBackend.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateCursostablev2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Nivel",
+                table: "Cursos",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Nivel",
+                table: "Cursos");
+        }
+    }
+}
---
+++ UniversityApiBackend/Models/DataModels/Curso.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UniversityApiBackend.Models.DataModels
+{
+    public class Curso : BaseEntity
+    {
+        public string Nombre { get; set; } = string.Empty;
+
+        [StringLength(280)]
+        public string DescripcionCorta { get; set; } = string.Empty;
+        public string DescripcionLarga { get; set; } = string.Empty;
+        public string PublicoObjetivo { get; set; } = string.Empty;
+        public string Objetivos { get; set; } = string.Empty;
+        public string Requisitos { get; set; } = string.Empty;
+        public Niveles Nivel { get; set; }
+
+        public enum Niveles
+        {
+            Básico,
+            Intermedio,
+            Avanzado
+        }
+
+    }
+}
